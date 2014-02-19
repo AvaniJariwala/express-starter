eb21dbdfddce386120bad67cdbf40b18ce023a98
Avani Jariwala <avanijariwala@Avanis-MacBook-Pro.local>
2014-02-18 23:24:58 -0800

    Version 15

--- static/js/calc.js
+++ static/js/calc.js
@@ -143,7 +143,7 @@ $(function() {
 
   $('#equals').click(function() {
     // FIXME
-    var res = eval('$('#output').text()');
+    var res = eval('outputNum');
     $('#output').text(res);
   });
 
