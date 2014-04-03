f7b0324ca2fdd8c450017cec5dc6c061d736903f
Avani Jariwala <avanijariwala@Avanis-MacBook-Pro.local>
2014-04-02 19:46:14 -0700

    Diago-ball!

--- static/js/ball.js
+++ static/js/ball.js
@@ -8,18 +8,22 @@ $(document).ready(function() {
   //PUT STUFF HERE
 
   //run an iteration of the game
-  var x = 20; 
+  var ball = {x: 30, y: 10, radius: 30}
+
+  var x = 30;
+  var y = 10;
 
   var updateGame = function() {
     context.fillStyle='pink';
     context.fillRect(0, 0, 800, 800);
     context.beginPath();
     context.fillStyle='red';
-    context.arc(x, 20, 20,0,2*Math.PI);
+    context.arc(ball.x, ball.y, ball.radius,0,2*Math.PI);
     context.closePath();
     context.stroke();
     context.fill();
-    x = x + 5;
+    ball.x = ball.x + 5;
+    ball.y = ball.y + 5;
     setTimeout(updateGame, 10);
   };
 
