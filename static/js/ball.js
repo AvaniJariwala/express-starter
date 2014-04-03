593f9ef0fd82942296a92de141636d0d6064db1b
Avani Jariwala <avanijariwala@Avanis-MacBook-Pro.local>
2014-04-02 20:02:19 -0700

    MOVING BALLZ!

--- static/js/ball.js
+++ static/js/ball.js
@@ -8,10 +8,12 @@ $(document).ready(function() {
   //PUT STUFF HERE
 
   //run an iteration of the game
-  var ball = {x: 30, y: 10, radius: 30}
+  var ball = {x: 30, y: 10, radius: 30, vx: 5, vy: 5}
 
   var x = 30;
   var y = 10;
+  var vx = 5;
+  var vy = 5;
 
   var updateGame = function() {
     context.fillStyle='pink';
@@ -22,8 +24,20 @@ $(document).ready(function() {
     context.closePath();
     context.stroke();
     context.fill();
-    ball.x = ball.x + 5;
-    ball.y = ball.y + 5;
+    ball.x = ball.x + vx;
+    ball.y = ball.y + vy;
+    if (ball.x === width) {
+      vx = -5;
+    }
+    if (ball.y === height) {
+      vy = -5;
+    }
+    if (ball.x === 0) {
+      vx = 5;
+    }
+    if (ball.y === 0) {
+      vy = 5;
+    }
     setTimeout(updateGame, 10);
   };
 
