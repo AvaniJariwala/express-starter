d4991287d46e4de9a9f7e538ffb864ca759f9f39
Avani Jariwala <avanijariwala@Avanis-MacBook-Pro.local>
2014-04-08 16:36:14 -0700

    Made Red Balls Appear!

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -7,8 +7,29 @@ $(document).ready(function() {
 
   // PUT STUFF HERE
 
+  var balls = new Array();
+
+  var b0 = {x: 300, y: 300, radius: 30, vx: 5, vy: 5}
+
+  var b1 = {x: 50, y: 50, radius: 40, vx: 5, vy: 5}
+  
+  var b2 = {x: 200, y: 100, radius: 50, vx: 5, vy: 5}
+
+  balls.push(b0);
+  balls.push(b1);
+  balls.push(b2);
+
   // Run an interation of the game
   var updateGame = function() {
+    for (var i = 0; i < balls.length; i++) {
+      context.beginPath();
+      context.fillStyle='red';
+      context.arc(balls[i].x, balls[i].y, balls[i].radius,0,2*Math.PI);
+      context.closePath();
+      context.stroke();
+      context.fill();
+    };
+    
     // PUT STUFF HERE
   };
 
