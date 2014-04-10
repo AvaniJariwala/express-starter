dee1502a38dd40b22f496c5648cea0820259993a
Avani Jariwala <avanijariwala@Avanis-MacBook-Pro.local>
2014-04-09 19:38:32 -0700

    Finished Multiple Random Ballz

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -7,48 +7,48 @@ $(document).ready(function() {
 
   // PUT STUFF HERE
 
-  var balls = new Array();
+  var numBalls = 3;
 
-  var b0 = {x: 300, y: 300, radius: 30, vx: 5, vy: 5}
-
-  var b1 = {x: 50, y: 50, radius: 40, vx: 5, vy: 5}
+  var balls = [];
   
-  var b2 = {x: 200, y: 100, radius: 50, vx: 5, vy: 5}
+  for (var i = 0; i <= numBalls; i++) {
+    a = {x: width*Math.random(), y: height*Math.random(), radius: 50*Math.random(), vx: 2.5*Math.random()+2.5, vy: 2.5*Math.random()+2.5};
+    balls.push(a);
+  }
 
-
-  balls.push(b0);
-  balls.push(b1);
-  balls.push(b2);
+  console.log(balls);
 
   // Run an interation of the game
   var updateGame = function() {
+    context.fillStyle='pink';
+    context.fillRect(0, 0, 800, 800);
+
     for (var i = 0; i < balls.length; i++) {
       balls[i].x = balls[i].x + balls[i].vx;
       balls[i].y = balls[i].y + balls[i].vy;
-      if (balls[i].x === width) {
+      if (balls[i].x >= width) {
         balls[i].vx = -5;
       }
-      if (balls[i].y === height) {
+      if (balls[i].y >= height) {
         balls[i].vy = -5;
       }
-      if (balls[i].x === 0) {
+      if (balls[i].x <= 0) {
         balls[i].vx = 5;
       }
-      if (balls[i].y === 0) {
+      if (balls[i].y <= 0) {
         balls[i].vy = 5;
       }
     }
-    context.fillStyle='pink';
-    context.fillRect(0, 0, 800, 800);
-    for (var i = 0; i < balls.length; i++) {
+    
+    for (var c = 0; c < balls.length; c++) {
       context.beginPath();
       context.fillStyle='red';
-      context.arc(balls[i].x, balls[i].y, balls[i].radius,0,2*Math.PI);
+      context.arc(balls[c].x, balls[c].y, balls[c].radius,0,2*Math.PI);
       context.closePath();
       context.stroke();
       context.fill();
-    };
-    setTimeout(updateGame, 10);
+    }
+    requestAnimationFrame(updateGame);
   };
 
   // Handle a canvas click event
